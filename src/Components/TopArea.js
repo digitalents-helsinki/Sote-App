052cc8706cee5dba2390115e8b085b58bdf8cbde
Thomas Hillebrink <thomas.hillebrink@digitalentshelsinki.fi>
2020-02-05 14:58:23 +0200

    Top area fix

--- src/Components/TopArea.js
+++ src/Components/TopArea.js
@@ -3,44 +3,27 @@ import backArrowSvg from "../Images/back-arrow.svg";
 import moreBtnSvg from "../Images/more-btn.svg";
 
 function TopArea({ setMenuVisibility, history }) {
-  if (true) {
-    return (
-      <div className="TopArea">
-        <div className="flex-container">
-          <img />
-          <h1 className="AppName">SoTe</h1>
-          <img
-            className="more-btn"
-            src={moreBtnSvg}
-            alt="moreBtnSvg"
-            onClick={() => setMenuVisibility(true)}
-          />
-        </div>
+  return (
+    <div className="TopArea">
+      <div className="flex-container">
+        <img
+          className="back-arrow"
+          src={backArrowSvg}
+          onClick={() => {
+            history.goBack();
+          }}
+          alt="backArrowSvg"
+        />
+        <h1 className="AppName">SoTe</h1>
+        <img
+          className="more-btn"
+          src={moreBtnSvg}
+          alt="moreBtnSvg"
+          onClick={() => setMenuVisibility(true)}
+        />
       </div>
-    );
-  } else {
-    return (
-      <div className="TopArea">
-        <div className="flex-container">
-          <img
-            className="back-arrow"
-            src={backArrowSvg}
-            onClick={() => {
-              history.goBack();
-            }}
-            alt="backArrowSvg"
-          />
-          <h1 className="AppName">SoTe</h1>
-          <img
-            className="more-btn"
-            src={moreBtnSvg}
-            alt="moreBtnSvg"
-            onClick={() => setMenuVisibility(true)}
-          />
-        </div>
-      </div>
-    );
-  }
+    </div>
+  );
 }
 
 export default TopArea;
