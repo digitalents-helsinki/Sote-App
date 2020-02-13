48838ddb5a1f98b5506ebc5c6aa96459b0dbeac1
Topias Rytkönen <topias.rytkonen@digitalentshelsinki.fi>
2020-02-13 15:39:53 +0200

    Close menupanel if user clicks outside of it

--- src/Components/MenuPanel.js
+++ src/Components/MenuPanel.js
@@ -1,6 +1,6 @@
 import React from "react";
 import Exit_icon from "../Images/exit-icon.svg";
-import { useHistory } from "react-router-dom";
+import { useHistory, useLocation } from "react-router-dom";
 
 function MenuPanel({
   setMenuVisibility,
@@ -10,49 +10,53 @@ function MenuPanel({
   testingdata
 }) {
   const history = useHistory();
+  const location = useLocation();
   return (
-    <div className={"menu-panel" + (menuVisibility ? " active" : "")}>
-      <img
-        onClick={() => setMenuVisibility(false)}
-        style={{ height: "27px", float: "right" }}
-        src={Exit_icon}
-        alt="exit-icon"
-      ></img>
-      <div className="content">
-        <div
-          className="reset"
-          onClick={() => {
-            history.replace("/");
-            setPersonData({});
-            setMenuVisibility(false);
-          }}
-        >
-          <p>Aloita alusta</p>
-        </div>
-        <div
-          className="emeregencyPage-fromMenu"
-          onClick={() => {
-            setEmergencyVisibility(true);
-            setMenuVisibility(false);
-          }}
-        >
-          <p>
-            Hätätilanne
-            <wbr />
-            oireet
-          </p>
-        </div>
-        <div
-          className="emeregencyPage-fromMenu"
-          onClick={() => {
-            setPersonData(testingdata);
-            setMenuVisibility(false);
-          }}
-        >
-          <p>DevOps: Activate testing Data</p>
+    <>
+      <div className="menu-panel-bg" onClick={() => setMenuVisibility(false)} />
+      <div className={"menu-panel" + (menuVisibility ? " active" : "")}>
+        <img
+          onClick={() => setMenuVisibility(false)}
+          style={{ height: "27px", float: "right" }}
+          src={Exit_icon}
+          alt="exit-icon"
+        ></img>
+        <div className="content">
+          <div
+            className="reset"
+            onClick={() => {
+              if (location.pathname !== "/") history.replace("/");
+              setPersonData({});
+              setMenuVisibility(false);
+            }}
+          >
+            <p>Aloita alusta</p>
+          </div>
+          <div
+            className="emeregencyPage-fromMenu"
+            onClick={() => {
+              setEmergencyVisibility(true);
+              setMenuVisibility(false);
+            }}
+          >
+            <p>
+              Hätätilanne
+              <wbr />
+              oireet
+            </p>
+          </div>
+          <div
+            className="emeregencyPage-fromMenu"
+            onClick={() => {
+              setPersonData(testingdata);
+              setMenuVisibility(false);
+            }}
+          >
+            <p>DevOps: Activate testing Data</p>
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 }
 
