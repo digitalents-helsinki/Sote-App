173edc64033729dcb1909d58d12df9139309572f
tounsa <toni.nieminen@windowslive.com>
2020-10-19 14:14:07 +0300

    Updated cell size and added tajunnantaso for editing

---
+++ src/Components/Tajunnantaso.js
@@ -0,0 +1,160 @@
+import React, { useRef } from "react";
+
+function TajunnantasoComponent(props) {
+    const newsRef = useRef(null);
+    //  const keyvalue = `${props.name} - NEWSscore`;
+  
+    function renderCells() {
+      function calculateActiveState(cell, idx) {
+        if (!props.personData[props.name] || cell === null) {
+          return "";
+        }
+        if (Array.isArray(cell)) {
+          if (
+            props.personData[props.name] >= cell[0] &&
+            props.personData[props.name] <= cell[1]
+          ) {
+            return "active";
+          }
+        } else if (
+          (!idx && props.personData[props.name] <= cell) ||
+          (idx && props.personData[props.name] >= cell)
+        ) {
+          return "active";
+        }
+        return "";
+      }
+  
+      return props.cells.map((cell, idx) => {
+        if (cell === null) {
+          return (
+            <div
+              className={`news-meter-cell n${idx + 1} ${calculateActiveState(
+                cell,
+                idx
+              )}`}
+              key={idx}
+            ></div>
+          );
+        } else if (Array.isArray(cell)) {
+          return (
+            <div
+              className={`news-meter-cell n${idx + 1} ${calculateActiveState(
+                cell,
+                idx
+              )}`}
+              key={idx}
+            >{`${cell[0]}-${cell[1]}`}</div>
+          );
+        } else {
+          return (
+            <div
+              className={`news-meter-cell n${idx + 1} ${calculateActiveState(
+                cell,
+                idx
+              )}`}
+              key={idx}
+            >{`${!idx ? "≤ " : "≥ "}${cell}`}</div>
+          );
+        }
+      });
+    }
+  
+    return (
+      <div className="news-container">
+        <input
+          className="news-input"
+          type="number"
+          placeholder={
+            props.personData[props.name] ? props.personData[props.name] : ""
+          }
+          onFocus={() => {
+            const scrollParent =
+              document.querySelector(".Page") ||
+              document.querySelector(".instruction-page-container");
+            const y =
+              newsRef.current.getBoundingClientRect().top +
+              (scrollParent.scrollY || scrollParent.scrollTop) -
+              (window.innerHeight ||
+                document.documentElement.clientHeight ||
+                document.body.clientHeight) /
+                1.8;
+            const scrollToNews = () => {
+              try {
+                scrollParent.scrollTo({ top: y, behavior: "smooth" });
+              } catch (err) {
+                console.log(err);
+                scrollParent.scrollTop = y;
+              }
+            };
+            scrollToNews();
+            // detect mobile keyboard popping up and then scroll
+            (function listenToWindowHeight(originalWindowHeight, startTime) {
+              if (
+                originalWindowHeight !==
+                (window.innerHeight ||
+                  document.documentElement.clientHeight ||
+                  document.body.clientHeight)
+              ) {
+                scrollToNews();
+              } else if (startTime + 2000 > Date.now()) {
+                setTimeout(
+                  listenToWindowHeight,
+                  0,
+                  originalWindowHeight,
+                  startTime
+                );
+              }
+            })(
+              window.innerHeight ||
+                document.documentElement.clientHeight ||
+                document.body.clientHeight,
+              Date.now()
+            );
+          }}
+          onChange={e => {
+            const inputValue = Number(e.target.value);
+  
+            const getNewsFromIndex = index => [3, 2, 1, 0, 1, 2, 3][index];
+            const NEWSscore = props.cells.reduce((score, cell, idx) => {
+              if (score !== null) return score;
+              if (cell) {
+                if (Array.isArray(cell)) {
+                  if (inputValue >= cell[0] && inputValue <= cell[1]) {
+                    return getNewsFromIndex(idx);
+                  }
+                } else if (idx === 0) {
+                  if (inputValue <= cell) return getNewsFromIndex(idx);
+                } else if (cell <= inputValue) return getNewsFromIndex(idx);
+              }
+              return score;
+            }, null);
+        
+            const keyvalue = `${props.name} - NEWSscore`;
+
+          const tsname = `${props.name}_timestamp`;
+          const ts = new Date().toLocaleTimeString(['en-GB'], {
+            hour: "2-digit",
+            minute: "2-digit"
+          });
+
+          const oldData = props.personData;
+          const newData = {
+            ...oldData,
+            [props.name]: inputValue,
+            [keyvalue]: NEWSscore,
+            [tsname]: ts
+          };
+          props.setPersonData(newData);
+
+          //--Update personData
+        }}
+      />
+      <div className="news-meter" ref={newsRef}>
+        {renderCells()}
+      </div>
+    </div>
+  );
+}
+
+export default TajunnantasoComponent;
--- src/Pages/TajunnantasoPage.js
+++ src/Pages/TajunnantasoPage.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Page from "../Components/Page";
 import DoubleButton from "../Components/DoubleButton";
+// import Tajunnantaso from "../Components/Tajunnantaso";
 
 function TajunnantasoPage({ personData, setPersonData }) {
   return (
@@ -14,6 +15,12 @@ function TajunnantasoPage({ personData, setPersonData }) {
         setPersonData={setPersonData}
         name={"Tajunnan taso"}
       />
+      {/* <Tajunnantaso
+        cells={["Normaali", "Poikkeava", null, null, null, null, null]}
+        name={"Mittaa verensokeri:"}
+        personData={personData}
+        setPersonData={setPersonData}
+      /> */}
     </Page>
   );
 }
