5bd523d18aa125b619d393f87e0f175d9c9ae414
tounsa <toni.nieminen@windowslive.com>
2021-01-21 16:23:42 +0200

    Add more to report

--- src/Pages/LastReportPage.js
+++ src/Pages/LastReportPage.js
@@ -1,6 +1,24 @@
 import React from "react";
 import Arrow from "../Images/arrow-white.svg";
 import helsinki from "../Images/helsinki.png";
+import reportimage from "../Images/report-overview.png";
+
+function myFunction() {
+  document.getElementById("dropdown").classList.toggle("show");
+}
+
+// window.onclick = function(event) {
+//   if (!event.target.matches('.report-button')) {
+//     let dropdowns = document.getElementsByClassName("dropdown-content");
+//     var i;
+//     for (i = 0; i < dropdowns.length; i++) {
+//       var openDropdown = dropdowns[i];
+//       if (openDropdown.classList.contains('show')) {
+//         openDropdown.classList.remove('show');
+//       }
+//     }
+//   }
+// }
 
 function LastReportPage({ setLastReportVisibility, visibility }) {
   return (
@@ -28,12 +46,38 @@ function LastReportPage({ setLastReportVisibility, visibility }) {
       </div>
       <div className="hatatilanne-container">
           <div className="report-header">
-              <p>report date</p>
+              <p>Raportti - 14/01/2021, 14:52</p>
               <div className="report-button">
                 <p>+</p>
               </div>
           </div>
-          <p>Test</p>
+          <div className="report-header" style={{color: "#ab2615"}}>
+              <p>Raportti - 14/01/2021, 13:20</p>
+              <div onClick={myFunction} className="report-button">
+                <p>!</p>
+              </div>
+          </div>
+          <div id="dropdown" className="dropdown-content">
+            <img src={reportimage} alt=""></img>
+          </div>
+          <div className="report-header">
+              <p>Raportti - 13/01/2021, 9:57</p>
+              <div className="report-button">
+                <p>+</p>
+              </div>
+          </div>
+          <div className="report-header">
+              <p>Raportti - 13/01/2021, 9:57</p>
+              <div className="report-button">
+                <p>+</p>
+              </div>
+          </div>
+          <div className="report-header">
+              <p>Raportti - 13/01/2021, 9:57</p>
+              <div className="report-button">
+                <p>+</p>
+              </div>
+          </div>
       </div>
     </div>
   );
