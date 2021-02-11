a9caf080ff7f56a4d61f4c811a2c9b968c9195f1
tounsa <toni.nieminen@windowslive.com>
2021-02-11 13:30:12 +0200

    Add button animation

--- src/Pages/LastReportPage.js
+++ src/Pages/LastReportPage.js
@@ -3,9 +3,23 @@ import Arrow from "../Images/arrow-white.svg";
 import helsinki from "../Images/helsinki.png";
 import reportimage from "../Images/report-overview.png";
 
-function myFunction() {
+function reportDropdown() {
   document.getElementById("dropdown").classList.toggle("show");
 }
+function reportDropdown2() {
+  document.getElementById("dropdown2").classList.toggle("show");
+}
+
+function circleButton() {
+  document.getElementById("circle-button").classList.toggle("opened");
+}
+function circleButton2() {
+  document.getElementById("circle-button2").classList.toggle("opened");
+}
+
+// $('.circle-plus').on('click', function(){
+//   $(this).toggleClass('opened');
+// })
 
 // window.onclick = function(event) {
 //   if (!event.target.matches('.report-button')) {
@@ -43,18 +57,27 @@ function LastReportPage({ setLastReportVisibility, visibility }) {
       </div>
       <div className="hatatilanne-container">
         <div className="report-header">
-          <p>Raportti - 14/01/2021, 14:52</p>
-          <div className="report-button">
-            <p>+</p>
-          </div>
-        </div>
-        <div className="report-header" style={{ color: "#ab2615" }}>
           <p>Raportti - 14/01/2021, 13:20</p>
-          <div onClick={myFunction} className="report-button">
-            <p>!</p>
+          <div onClick={() => {reportDropdown(); circleButton();}} id="circle-button" className="report-button circle-plus closed">
+            <div class="circle">
+              <div class="horizontal"></div>
+              <div class="vertical"></div>
+            </div>
           </div>
         </div>
         <div id="dropdown" className="dropdown-content">
+          <img src={reportimage} alt=""></img>
+        </div>
+        <div className="report-header" style={{ color: "#ab2615" }}>
+          <p>Raportti - 14/01/2021, 14:52</p>
+          <div onClick={() => {reportDropdown2(); circleButton2();}} id="circle-button2" className="report-button circle-plus closed">
+            <div class="circle">
+              <div class="horizontal"></div>
+              <div class="vertical"></div>
+            </div>
+          </div>
+        </div>
+        <div id="dropdown2" className="dropdown-content">
           <img src={reportimage} alt=""></img>
         </div>
         <div className="report-header">
